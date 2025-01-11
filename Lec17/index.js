7ec453912a6ba7eb0a011e1d1756eacaf12b300f
adarshchaubey006 <adarshchaubey006@gmail.com>
2025-01-11 20:53:42 +0530

    code

---
+++ Lec17/index.js
@@ -0,0 +1,33 @@
+// console.log('code');
+// number 
+// a=5.6
+// String 'c' 'cc' "ccc"
+// Boolean true,false,0,1
+// null
+// undefined
+// Symbol
+// BigInt
+
+// var , let , const
+// var a=5
+// console.log(a);
+// var a=5
+// var a=10
+// console.log(a);
+// let a=5;
+// console.log(a);
+
+// const c=100
+// c=3000
+// console.log(c);
+
+// console.log(0.1+0.2=0.3);
+
+// var a=100
+// var to=a==100
+// console.log(to);
+
+// let a='rahul'
+// let b='jain'
+// console.log(`${a}   ${b}`);
+
