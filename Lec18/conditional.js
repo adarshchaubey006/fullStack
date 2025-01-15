f01e7deff53a26c5ed194f3ce49e133182904fec
adarshchaubey006 <adarshchaubey006@gmail.com>
2025-01-15 19:42:24 +0530

    code

---
+++ Lec18/conditional.js
@@ -0,0 +1,39 @@
+
+// if(a=false){
+//     console.log('true');
+// }
+// else{
+//     console.log('false'); 
+// }
+// let a=false
+// a?console.log('hello'):console.log('hiii');
+
+// function sum(a=40,b=50){
+// console.log(a+b);
+// }
+// sum(5,6)
+
+// const call=() =>   {
+//     console.log(5+5);
+// }
+// call();
+
+// function sum(){
+//     console.log();
+//     return hello;
+// }
+// sum();
+// console.log(sum());
+// let data=sum();
+// console.log(data);
+
+// for(var i=0;i<=5;i++){
+// }
+
+// let arrr=[1,2,'hello',null,undefined]
+// for(let i in arrr){
+//     console.log(i);
+// }
+// for(let i of arrr){
+//     console.log(i);
+// }
